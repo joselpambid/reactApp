ec03ada679837d68e43a446d1b5271fd0ca5861b
unknown <pambid.josel@gmail.com>
2020-08-13 16:07:01 +0800

    Updates

---
+++ precache-manifest.3d7c41284abf23e9d2e468f0bd10384a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dde7f3cdeb96537b1645973953929a9",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "0f6371bed954c09ba83c",
+    "url": "/reactApp/static/css/main.8de35aa6.chunk.css"
+  },
+  {
+    "revision": "22a02db674315071403b",
+    "url": "/reactApp/static/js/2.33c831b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactApp/static/js/2.33c831b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6371bed954c09ba83c",
+    "url": "/reactApp/static/js/main.1197d73b.chunk.js"
+  },
+  {
+    "revision": "91e06702626d120f1a82",
+    "url": "/reactApp/static/js/runtime-main.ace65657.js"
+  }
+]);
